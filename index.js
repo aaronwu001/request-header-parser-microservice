1c9cb07272a4d73846a43fdd8f0860791bf6ed75
Aaron Wu <aaronwu000@gmail.com>
2024-06-14 17:38:39 +0800

    Initial commit

--- index.js
+++ index.js
@@ -20,9 +20,20 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipaddress, language, software });
 });
+
+// {
+//   "ipaddress": "172.70.230.201",
+//   "language": "zh-TW,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-CN;q=0.5",
+//   "software": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0"
+// }
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
